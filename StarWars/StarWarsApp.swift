6128299988f0bc0b859f0ff7fc8c728fea219df6
Abhijeet Banarase <Abhijeet.Banarase@derivco.co.uk>
2024-09-27 21:08:57 +0100

    Initial Commit

---
+++ StarWars/StarWarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsApp.swift
+//  StarWars
+//
+//  Created by Abhijeet Banarase on 27/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
